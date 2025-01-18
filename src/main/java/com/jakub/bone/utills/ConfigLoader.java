f1af724b71af91297863a6592909435fd3c9b72b
jakubBone <jakub.bone1990@gmail.com>
2025-01-18 14:02:08 +0100

    refactor: improved readability

--- src/main/java/com/jakub/bone/utills/ConfigLoader.java
+++ src/main/java/com/jakub/bone/utills/ConfigLoader.java
@@ -10,11 +10,11 @@ public class ConfigLoader {
     static {
         try (InputStream input = ConfigLoader.class.getClassLoader().getResourceAsStream("config.properties")) {
             if (input == null) {
-                throw new RuntimeException("Configuration file 'config.properties' not found in classpath.");
+                throw new RuntimeException("Configuration file 'config.properties' not found in classpath");
             }
             properties.load(input);
         } catch (IOException e) {
-            throw new RuntimeException("Failed to load configuration file.", e);
+            throw new RuntimeException("Failed to load configuration file", e);
         }
     }
 
